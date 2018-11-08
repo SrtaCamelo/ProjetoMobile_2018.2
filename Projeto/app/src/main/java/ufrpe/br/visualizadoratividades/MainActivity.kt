6fb6c53799c2cafd251388d187a5d7d8c65ff8db
Diego <diego_rafael_97@hotmail.com>
2018-11-08 03:14:15 -0300

    algumas coisas

--- Projeto/app/src/main/java/ufrpe/br/visualizadoratividades/MainActivity.kt
+++ Projeto/app/src/main/java/ufrpe/br/visualizadoratividades/MainActivity.kt
@@ -1,12 +1,21 @@
 package ufrpe.br.visualizadoratividades
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById(R.id.button) as Button
+        button.setOnClickListener{
+            val intent = Intent(this, atActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- Projeto/app/src/main/java/ufrpe/br/visualizadoratividades/atActivity.kt
+++ Projeto/app/src/main/java/ufrpe/br/visualizadoratividades/atActivity.kt
@@ -1,4 +1,19 @@
 package ufrpe.br.visualizadoratividades
 
-class atActivity {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.atividades_tela.*
+
+class atActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.atividades_tela)
+
+
+    }
 }
