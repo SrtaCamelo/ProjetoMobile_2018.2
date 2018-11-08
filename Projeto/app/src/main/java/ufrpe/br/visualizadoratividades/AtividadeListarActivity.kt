38d886a53f59bac1f0ca8ebb5fd873f3cc0f5b6e
Maria Fernanda <fsandrade25@gmail.com>
2018-11-08 07:59:38 -0300

    Inicio de telas
    Criação da tela de listar atividade e mudança no nome das telas de atividade (adicionei ao padrão esperado)

---
+++ Projeto/app/src/main/java/ufrpe/br/visualizadoratividades/AtividadeListarActivity.kt
@@ -0,0 +1,12 @@
+package ufrpe.br.visualizadoratividades
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AtividadeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_atividade)
+    }
+}
