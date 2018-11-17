c55416b29f74a5e312351baa81025cd1e9e6dbc9
Maria Fernanda <fsandrade25@gmail.com>
2018-11-17 20:07:54 -0300

    Adicionado novas strings e mudança na tela de atividade

--- Projeto/app/src/main/java/ufrpe/br/visualizadoratividades/ListarAtividadeActivity.kt
+++ Projeto/app/src/main/java/ufrpe/br/visualizadoratividades/ListarAtividadeActivity.kt
@@ -4,10 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
-import ufrpe.br.visualizadoratividades.fragments.FragmentCursos
-import ufrpe.br.visualizadoratividades.fragments.FragmentEsportes
-import ufrpe.br.visualizadoratividades.fragments.FragmentIdiomas
-import ufrpe.br.visualizadoratividades.fragments.FragmentOutras
+import android.widget.TableLayout
+import ufrpe.br.visualizadoratividades.fragments.*
 
 class ListarAtividadeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,16 +16,17 @@ class ListarAtividadeActivity : AppCompatActivity() {
         var fragmentEsportes = FragmentEsportes()
         var fragmentIdiomas = FragmentIdiomas()
         var fragmentOutras = FragmentOutras()
-
-        var adapter_var : ViewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
-
+        var fragmentTodas = FragmentTodasAtividades()
+        var adapter_var = ViewPagerAdapter(supportFragmentManager)
         var vp_listarAtividade : ViewPager = findViewById(R.id.viewpager_id)
         var tb_listarAtividade : TabLayout = findViewById(R.id.tbAtividadeListar)
 
-        adapter_var.AddFragment(fragmentCursos, "Cursos")
-        adapter_var.AddFragment(fragmentEsportes, "Esportes")
-        adapter_var.AddFragment(fragmentIdiomas, "Idiomas")
-        adapter_var.AddFragment(fragmentOutras, "Outros")
+
+        adapter_var.AddFragment(fragmentTodas, getString(R.string.todos))
+        adapter_var.AddFragment(fragmentCursos, getString(R.string.cursos))
+        adapter_var.AddFragment(fragmentEsportes, getString(R.string.esportes))
+        adapter_var.AddFragment(fragmentIdiomas, getString(R.string.idiomas))
+        adapter_var.AddFragment(fragmentOutras, getString(R.string.outros))
 
         vp_listarAtividade.adapter = adapter_var
         tb_listarAtividade.setupWithViewPager(vp_listarAtividade)
---
+++ Projeto/app/src/main/java/ufrpe/br/visualizadoratividades/fragments/FragmentTodasAtividades.kt
@@ -0,0 +1,16 @@
+package ufrpe.br.visualizadoratividades.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ufrpe.br.visualizadoratividades.R
+
+class FragmentTodasAtividades : Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view : View = inflater.inflate(R.layout.todas_atividades_fragment, container, false)
+        return view
+    }
+}
